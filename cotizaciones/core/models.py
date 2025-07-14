c427b8e138b1aa69c35bedff095cbe27b6e73b66
lhuayhuac <lhuayhuac@unsa.edu.pe>
2025-07-13 20:23:15 -0500

    crear un modelo de usuario personalizado

--- cotizaciones/core/models.py
+++ cotizaciones/core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UsuarioManager(BaseUserManager):
+    def create_user(self, userid, password=None, **extra_fields):
+        if not userid:
+            raise ValueError('El usuario debe tener un userid')
+        user = self.model(userid=userid, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, userid, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(userid, password, **extra_fields)
+
+class Usuario(AbstractBaseUser):
+    userid = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    loginstatus = models.CharField(max_length=20, default='offline')
+    registerdate = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'userid'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UsuarioManager()
+
+    def __str__(self):
+        return self.userid
+
+    def verifiLogin(self, password):
+        return self.check_password(password)
