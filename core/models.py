1b9223edd98797d6671ab588e79893069d05a0d7
MarianaVLJ <mluisj@unsa.edu.pe>
2025-07-21 13:13:08 -0500

    Agregar modelo PerfilCliente relacionado con User para almacenar datos adicionales como RUC/DNI, dirección y teléfono

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PerfilCliente(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ruc_dni = models.CharField(max_length=20, unique=True)
+    direccion = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
