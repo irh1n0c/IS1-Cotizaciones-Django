546c67574f6758e80d0cf8e1e532dfb0dbe5cfef
MarianaVLJ <mluisj@unsa.edu.pe>
2025-07-22 15:28:59 -0500

    se modifica el nombre de la variable ruc_dni a ruc

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class PerfilCliente(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ruc_dni = models.CharField(max_length=20, unique=True)
+    ruc = models.CharField(max_length=20, unique=True)
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=15)
 
